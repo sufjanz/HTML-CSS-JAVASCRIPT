5b389b55a6558ca1a5edd34fcb90cab37023020c
sufjanz <sufjanzogaj@gmail.com>
2025-10-02 20:46:43 +0200

    moduli 28

---
+++ Moduli 28/main.js
@@ -0,0 +1,34 @@
+var buttonation = document.getElementById("hyu");
+
+buttonation.onclick = function(){
+    alert("You've been scammed!Send another 500$ to get your files back")
+}
+
+var buttonation2 = document.getElementById("hyuyu");
+var buttonation3 = document.getElementById("hyuyuy");
+var buttonation4 = document.getElementById("hyuyuyu");
+
+var buttonation1 = document.querySelector("#hyuy")
+buttonation1.addEventListener ('click', function(){
+    alert("You've been scammed!Send another 500$ to get your files back")
+})
+
+buttonation2.onmouseover = function(){
+    alert("Alertay")
+}
+
+buttonation3.onmouseleave = function(){
+    alert("Alertay")
+}
+
+buttonation4.onmousewheel = function(){
+    alert("Alertay")
+}
+
+var btn1 = document.getElementById("bt1");
+var btn2 = document.getElementById("bt2");
+var text = document.getElementById("txt1");
+
+btn1.onclick = function(){
+    text.style.color="red"
+}
