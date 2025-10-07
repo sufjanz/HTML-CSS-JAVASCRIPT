3bce4c44fec531b87b76073b1fcc1a4633d4cad4
sufjanz <sufjanzogaj@gmail.com>
2025-10-07 20:45:28 +0200

    Moduli 28

--- Moduli 28/main.js
+++ Moduli 28/main.js
@@ -28,7 +28,15 @@ buttonation4.onmousewheel = function(){
 var btn1 = document.getElementById("bt1");
 var btn2 = document.getElementById("bt2");
 var text = document.getElementById("txt1");
+var txt2 = document.getElementById("txt2")
 
 btn1.onclick = function(){
-    text.style.color="red"
+    text.style.color = "#c7c7c7";
+    text.style.textAlign = "center";
+    text.style.backgroundColor = "#d4d4e5";
+    text.style.fontSize = "200px";
+    txt2.setAttribute("class", "txt2")
 }
+
+// btn1.onclick = function(){
+// }
