d390dfbd434decbf36d0666126880ab46086be42
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-11-13 20:40:03 +0100

    Moduli 35

---
+++ Moduli 35/main.js
@@ -0,0 +1,25 @@
+function openMenu(){
+    var menu=document.getElementById("myLinks");
+    if (menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+}
+
+function search(){
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("MyInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("MyList")
+    li = document.getElementsByTagName("li");
+    for(i = 0; i<li.length;i++){
+        a = li[i];
+        txtValue = a.textContent;
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
+            li[i].style.display = "block";
+        }
+        }else{
+            li[i].style.display = "none";
+    }
+}
