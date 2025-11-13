ce6cffae1930e10376f84720f7b4965d3b6461a2
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-11-13 20:42:30 +0100

    Moduli 35

--- Moduli 35/main.js
+++ Moduli 35/main.js
@@ -9,17 +9,16 @@ function openMenu(){
 
 function search(){
     var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("MyInput");
+    input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("MyList")
-    li = document.getElementsByTagName("li");
+    ul = document.getElementById("myList")
+    li = ul.getElementsByTagName("li");
     for(i = 0; i<li.length;i++){
         a = li[i];
         txtValue = a.textContent;
         if(txtValue.toUpperCase().indexOf(filter) > -1){
             li[i].style.display = "block";
-        }
         }else{
             li[i].style.display = "none";
     }
-}
+}}
