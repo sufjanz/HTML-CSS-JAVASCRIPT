e9c8834c91f508ff87dbc05406bb4f6d4c410b0b
sufjanz <sufjanzogaj@gmail.com>
2025-11-20 20:41:57 +0100

    Moduli 37

---
+++ Moduli 37/js/main.js
@@ -0,0 +1,32 @@
+$(window).ready(function(){
+    alert('It is working!!!');
+});
+
+
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+
+$('a.test:first');
+$('tr:odd');
+$('#myForm :input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
+
+
+$('#sort').sortable();
+$(function(){
+    $('#accordion').accordion({
+        collapsible: true
+    });
+})
+
+$(function(){
+    $('#tabs').accordion({
+        collapsible: true
+    });
+})
